2eb8b7366d8d580444cc0ca0a3385cac78e5c95e
alandysson <alandyssom@gmail.com>
2022-08-05 14:45:47 -0300

    created crud with laravel:livewire

---
+++ app/Http/Livewire/Expense/ExpenseList.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Livewire\Expense;
+
+use Livewire\Component;
+use App\Models\Expense;
+
+class ExpenseList extends Component
+{
+    public function render()
+    {
+        $expenses = auth()->user()->expenses()->count() ? auth()->user()->expenses()->orderBy('created_at', 'DESC')->paginate(2) : [];
+        return view('livewire.expense.expense-list', compact('expenses'));
+    }
+
+    public function remove($expense){
+        $exp = auth()->user()->expenses()->find($expense);
+        $exp->delete();
+
+        session()->flash('message', 'Registro excluido com sucesso!');
+    }
+}
